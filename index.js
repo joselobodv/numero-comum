cd9d965d95033911919a5b863d599f2adeae8d3c
Jose augusto <joseaugusto0242@gmail.com>
2024-06-19 09:54:25 -0300

    feat: implementa calculo

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let entrada;
+let numero1;
+let numero2;
 
-console.log("Digite dois numeros:");
 process.stdin.on("data", function (data) {
-  let input = data.toString().trim().toLowerCase();
-  if (resposta) {
-    for (let i = maximo; i <= mmc * mmc; i++) {
-      if (i % num1 === 0 && i % num2 === 0) {
-        mmc = i;
+  let input = +data.toString().trim();
+
+  if (!numero1) {
+    numero1 = input;
+  } else {
+    numero2 = input;
+    if(numero1 > numero2){
+      i = numero1
+    }else{
+      i = numero2
+    }
+    for(input; i <= numero1 * numero2; i++){
+      if(i% numero1 == 0  && i % numero2 == 0){
+        console.log("MMC: " + i)
         break;
       }
     }
